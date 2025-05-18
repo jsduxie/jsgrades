b0fbe7c827d140e8f3f8bd0e71c159193c7935f9
James Duxbury <james.s.duxbury@durham.ac.uk>
2025-05-18 02:06:29 +0100

    [Fix] Register/Login Error Handling + Linting
    Resolve linting + prettier errors
    
    Fix error handling for Login to mark err as unknown and guard for error type
    
    Implement error handling for Register in line with login
    
    Need to handle specific Firebase errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import { AuthProvider } from "./context/AuthContext";
-import { useRoutes } from "react-router-dom";
+import { useRoutes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   const routesArray = [
-    { path: "*", element: <Login /> },
-    { path: "/login", element: <Login /> },
-    { path: "/register", element: <Register /> },
-    { path: "/home", element: <Home /> },
+    { path: '*', element: <Login /> },
+    { path: '/login', element: <Login /> },
+    { path: '/register', element: <Register /> },
+    { path: '/home', element: <Home /> },
   ];
-  let routesElement = useRoutes(routesArray);
+  const routesElement = useRoutes(routesArray);
 
   return (
     <AuthProvider>
--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,52 +1,51 @@
 // Allows for authentication to be tracked across components
 
-import React, { ReactNode, useContext, useEffect, useState } from "react";
-import { onAuthStateChanged, User } from "firebase/auth";
-import { auth } from "../firebase/Firebase";
+import { onAuthStateChanged, User } from 'firebase/auth';
+import React, { ReactNode, useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase/Firebase';
 
 type AuthContextType = {
-    currentUser: User | null;
-    userLoggedIn: boolean;
-    loading: boolean;
-}
+  currentUser: User | null;
+  userLoggedIn: boolean;
+  loading: boolean;
+};
 
-const AuthContext = React.createContext<AuthContextType | undefined >(undefined);
+const AuthContext = React.createContext<AuthContextType | undefined>(undefined);
 
 export function useAuth() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
 
-export function AuthProvider({ children }: {children: ReactNode}) {
-    const [currentUser, setCurrentUser] = useState<User | null>(null);
-    const [userLoggedIn, setUserLoggedIn] = useState(false);
-    const [loading, setLoading] = useState(true);
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, initialiseUser);
-        return unsubscribe;
-    }, [])
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, initialiseUser);
+    return unsubscribe;
+  }, []);
 
-    function initialiseUser(user: User | null) {
-        console.log("initialiseUser called with user:", user);
-        if (user) {
-            setCurrentUser(user);
-            setUserLoggedIn(true);
-        } else {
-            setCurrentUser(null);
-            setUserLoggedIn(false);
-        }
-        setLoading(false);
+  function initialiseUser(user: User | null) {
+    if (user) {
+      setCurrentUser(user);
+      setUserLoggedIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLoggedIn(false);
     }
+    setLoading(false);
+  }
 
-    const value = {
-        currentUser,
-        userLoggedIn,
-        loading
-    }
+  const value = {
+    currentUser,
+    userLoggedIn,
+    loading,
+  };
 
-    return (
-        <AuthContext.Provider value={value}>
-            {loading ? <div>Loading...</div> : children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={value}>
+      {loading ? <div>Loading...</div> : children}
+    </AuthContext.Provider>
+  );
 }
