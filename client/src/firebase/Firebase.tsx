a07b11f2ab68a5b5ba05058579a9936a5f8aaa5a
James Duxbury <james.s.duxbury@durham.ac.uk>
2025-05-23 21:28:49 +0100

    [Fix] Firebase.tsx
    Update erroneous ProcessingInstruction x2 with process to correctly pull data from .env

--- client/src/firebase/Firebase.tsx
+++ client/src/firebase/Firebase.tsx
@@ -9,8 +9,8 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_projectId,
   storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
   messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
-  appId: ProcessingInstruction.env.REACT_APP_FIREBASE_appId,
-  measurementId: ProcessingInstruction.env.REACT_APP_FIREBASE_measurementId,
+  appId: process.env.REACT_APP_FIREBASE_appId,
+  measurementId: process.env.REACT_APP_FIREBASE_measurementId,
 };
 
 // Initialize Firebase
