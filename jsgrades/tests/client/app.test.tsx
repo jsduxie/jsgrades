6daab2dd7cbc95ab7b646fa9a113804167e0cd57
James Duxbury <james.s.duxbury@durham.ac.uk>
2025-07-28 20:55:59 +0100

    12-next-js prettier formatting

--- jsgrades/tests/client/app.test.tsx
+++ jsgrades/tests/client/app.test.tsx
@@ -2,7 +2,7 @@ describe('Client Testing Setup', () => {
     it('should have required globals for testing', () => {
         expect(global.TextEncoder).toBeDefined();
         expect(global.TextDecoder).toBeDefined();
-        
+
         const testObj = { test: 'value' };
         expect(testObj.test).toBe('value');
     });
