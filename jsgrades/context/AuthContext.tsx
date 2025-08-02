3e4f841f94e1f65beb0558d5238e17e28a2d9dcd
James Duxbury <james.s.duxbury@durham.ac.uk>
2025-08-02 18:48:55 +0100

    issue-14-qualifications fix auth not routing to /, combine CI workflows

--- jsgrades/context/AuthContext.tsx
+++ jsgrades/context/AuthContext.tsx
@@ -56,6 +56,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
                 setCurrentUser(null);
                 setUserLoggedIn(false);
                 setUserDetails(null);
+                router.push('/');
             }
 
             setLoading(false);
